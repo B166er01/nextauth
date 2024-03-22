4a9fe0dfb989017c3fa50685d50c7dce72b99db1
B166er01 <b166erpro@gmail.com>
2024-03-22 11:12:43 +0100

    first commit

--- types/index.ts
+++ types/index.ts
@@ -13,3 +13,19 @@ import {
   export type IForgotPasswordSchema = z.infer<typeof forgotPasswordSchema>
   export type IResetPasswordSchema = z.infer<typeof resetPasswordSchema>
   
+
+  // Ripple button
+export interface IRippleButtonProps {
+  text: string
+  buttonClasses?: string
+  onClick?: (event: MouseEvent<HTMLButtonElement>) => void
+  type?: 'button' | 'submit' | 'reset' | undefined
+  icon?: ReactNode
+  isLoading?: boolean
+}
+export interface IRipple {
+  x: number
+  y: number
+  size: number
+  id: number
+}
